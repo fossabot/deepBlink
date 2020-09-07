7008c25d2893c8c25724d0df15313cfd101e942d
BBQuercus <bastian@eichenbergers.ch>
2020-09-07 14:59:34 +0200

    :paper: Squeeze and excitation trial implementation.
    Squeeze and excitation networks / blocks sound very interesting.
    This is a basic block implementing the original publication.

---
+++ deepblink/networks/squeeze.py
@@ -0,0 +1,22 @@
+"""Squeeze and excitation block implementation."""
+
+import tensorflow as tf
+
+
+# TODO: add input and type checks
+def squeeze_block(x: tf.keras.layers.Layer, ratio: int = 8):
+    """Squeeze and excitation block.
+
+    Args:
+        x: Input tensor.
+        ratio: Number of output filters.
+    """
+    filters = x.shape[-1]
+
+    x1 = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x1 = tf.keras.layers.Dense(max(filters // ratio, 1))(x1)
+    x1 = tf.keras.layers.Activation("relu")(x1)
+    x1 = tf.keras.layers.Dense(filters)(x1)
+    x1 = tf.keras.layers.Activation("sigmoid")(x1)
+    x = tf.keras.layers.Multiply()([x1, x])
+    return x
