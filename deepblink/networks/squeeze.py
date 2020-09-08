8dcd12963ed9281937b41aad568e963ee906ca73
BBQuercus <bastian@eichenbergers.ch>
2020-09-08 10:30:53 +0200

    :pencil: Add reference to squeeze docstring

--- deepblink/networks/squeeze.py
+++ deepblink/networks/squeeze.py
@@ -7,6 +7,8 @@ import tensorflow as tf
 def squeeze_block(x: tf.keras.layers.Layer, ratio: int = 8):
     """Squeeze and excitation block.
 
+    ref: https://arxiv.org/pdf/1709.01507.pdf.
+
     Args:
         x: Input tensor.
         ratio: Number of output filters.
